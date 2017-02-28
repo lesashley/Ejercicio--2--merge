e484cbd31b19a82cdc252dbc7e670c392cd91744
Leslie A. Lopez <lesashley56@gmail.com>
2017-02-28 17:17:30 -0500

    No sale la funcion calcular

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,21 @@
-var nombre = document.getElementById("nombre").value;
-var peso = document.getElementById("peso").value;
-var talla = document.getElementById("talla").value;
-var boton = document.getElementById("enviar");
-var show = document.getElementById("resultado").value;
+
+function calcular(){
+  var nombre = document.getElementById("nombre").value;
+  var peso = document.getElementById("peso").value;
+  var talla = document.getElementById("talla").value;
+
+  document.getElementById("resultado").value = "Nombre: " + nombre + "\n"+
+      "Indice de masa corporal (IMC): " + "\n"+ Math.floor(peso/Math.pow(talla,2));
+}
+var cajaVacia=document.getElementById("leer");
+document.getElementById("enviar").onclick=function resultado() {
+  cajaVacia.innerText=
+  "Composición corporal         	Índice de masa corporal (IMC)" + "\n"+
+  "Peso inferior al normal	      Menos de 18.5" + "\n"+
+  "Normal	                        18.5 – 24.9" + "\n" +
+  "Peso superior al normal	      25.0 – 29.9" + "\n"+
+  "Obesidad	                      Más de 30.0";
+}
+document.getElementById("form").onsubmit=function(e){
+  e.preventDefault();
+}
