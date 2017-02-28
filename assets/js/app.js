2b67a77f074de048a3ab0ff6e003ec5c77162189
Leslie A. Lopez <lesashley56@gmail.com>
2017-02-28 15:32:05 -0500

    Interfaz de la pagina sin funciones

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+var nombre = document.getElementById("nombre").value;
+var peso = document.getElementById("peso").value;
+var talla = document.getElementById("talla").value;
+var boton = document.getElementById("enviar");
+var show = document.getElementById("resultado").value;
