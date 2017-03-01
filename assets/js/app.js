0be8af8f917cafa6a11075e6237204fb56f5110c
Leslie A. Lopez <lesashley56@gmail.com>
2017-03-01 10:05:35 -0500

    Todas las funciones agregadas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,17 @@
 
-function calcular(){
+var cajaVacia = document.getElementById("leer");
+document.getElementById("enviar").onclick=function calcular() {
   var nombre = document.getElementById("nombre").value;
   var peso = document.getElementById("peso").value;
   var talla = document.getElementById("talla").value;
 
   document.getElementById("resultado").value = "Nombre: " + nombre + "\n"+
-      "Indice de masa corporal (IMC): " + "\n"+ Math.floor(peso/Math.pow(talla,2));
-}
-var cajaVacia=document.getElementById("leer");
-document.getElementById("enviar").onclick=function resultado() {
-  cajaVacia.innerText=
-  "Composición corporal         	Índice de masa corporal (IMC)" + "\n"+
-  "Peso inferior al normal	      Menos de 18.5" + "\n"+
-  "Normal	                        18.5 – 24.9" + "\n" +
-  "Peso superior al normal	      25.0 – 29.9" + "\n"+
-  "Obesidad	                      Más de 30.0";
+      "Indice de masa corporal (IMC): " + "\n"+ Math.floor(peso/Math.pow((talla/100),2));
+
+  cajaVacia.innerHTML="<pre>  Peso inferior al normal     "+  "Menos de 18.5<br>"+
+  "Normal	                    18.5 – 24.9<br>"+
+  "Peso superior al normal	    25.0 – 29.9<br>"+
+  "Obesidad	            Más de 30.0</pre>";
 }
 document.getElementById("form").onsubmit=function(e){
   e.preventDefault();
