3d1552fc7ecdb540a768b99851d159cca6d9bd35
lesashley <lesashley56@gmail.com>
2017-03-05 06:25:49 -0500

    Actualizando

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,29 @@
 
-function calcular(){
+document.getElementById("enviar").onclick=function calcular(){
   var nombre = document.getElementById("nombre").value;
   var peso = document.getElementById("peso").value;
   var talla = document.getElementById("talla").value;
-
-  document.getElementById("resultado").value = "Nombre: " + nombre + "\n"+
+    if(typeof(nombre) == "string" && typeof(talla) == "number"  ){
+      document.getElementById("resultado").value = "Nombre: " + nombre + "\n"+
       "Indice de masa corporal (IMC): " + "\n"+ Math.floor(peso/Math.pow(talla,2));
+    } else{
+      document.getElementById("resultado").value="Datos incorrectos";
+    }
 }
+
+
+/*
 var cajaVacia=document.getElementById("leer");
 document.getElementById("enviar").onclick=function resultado() {
   cajaVacia.innerText=
-  "Composición corporal         	Índice de masa corporal (IMC)" + "\n"+
-  "Peso inferior al normal	      Menos de 18.5" + "\n"+
-  "Normal	                        18.5 – 24.9" + "\n" +
-  "Peso superior al normal	      25.0 – 29.9" + "\n"+
-  "Obesidad	                      Más de 30.0";
+  "Composición corporal         "      +   	"Indice de masa corporal (IMC)" + "\n"+
+  "Peso inferior al normal      " + "Menos de 18.5" + "\n"+
+  "Normal                       " + "18.5 – 24.9" + "\n" +
+  "Peso superior al normal      "	+  "25.0 – 29.9" + "\n"+
+  "Obesidad                     " +  " Más de 30.0";
 }
+*/
+
 document.getElementById("form").onsubmit=function(e){
   e.preventDefault();
 }
