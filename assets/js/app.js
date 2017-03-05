494fb36d5f49c5fc2b2a1c722e505860f05fcc5a
miriampc <miiriampc@gmail.com>
2017-03-05 11:05:54 -0500

    implementando funcionalidad de calculo IMC

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,10 @@
-var nombre = document.getElementById("nombre").value;
-var peso = document.getElementById("peso").value;
-var talla = document.getElementById("talla").value;
-var boton = document.getElementById("enviar");
-var show = document.getElementById("resultado").value;
+document.getElementById('enviar').onclick = function calcularImc(){
+  var nombre = document.getElementById("nombre").value;
+  var peso = document.getElementById("peso").value;
+  var talla = document.getElementById("talla").value;
+  var show = document.getElementById("resultado");
+  show.innerHTML="Hola "+nombre+" tu indice de masa corporal es: "+peso/(talla*talla).toFixed(2);
+}
+document.getElementById('form').onsubmit=function(e){
+  e.preventDefault();
+}
